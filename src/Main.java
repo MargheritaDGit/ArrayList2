517dc63d9a6ea4ab443f7e7c35ff7192ec0fc20f
Margherita <margherita.deodato14@gmail.com>
2023-11-21 15:36:28 +0100

    commit arraylist2

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+/*Creare una classe Student che accenti nel costruttore il parametro name (String e age Int)
+  In questa classe avere i metodi pubblici per ottenere le informazioni.
+  Creare un ArrayList con 12 elementi e stamparlo in console.
+  Mettere in ordine la collezione e stampare il risultato*/
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student student = new Student("Margherita", 29);
+
+        List<Student> list = Arrays.asList(student);
+        List<String> lista1 = new ArrayList<>();
+        lista1.add(student.name);
+        lista1.add(String.valueOf(student.age));
+        lista1.add("H 1.60");
+        lista1.add("Occhi marroni");
+        lista1.add("Capelli castani");
+        lista1.add("id: 934");
+        lista1.add("Colore: rosso");
+        lista1.add("Film: Harry Potter");
+        lista1.add("Casata: Griffondoro");
+        lista1.add("Diploma: Hogwarts");
+
+        System.out.println("Studente aggiornato: " + lista1);
+
+        Collections.sort(lista1);
+        System.out.println("L'ordine alfabetico è " + lista1);
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,28 @@
+public class Student {
+
+
+    public String name;
+    public int age;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
